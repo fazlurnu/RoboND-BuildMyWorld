13634b9e56d63621b7570538efe505dfcbf5d74b
fazlurnu <fazlur.rahman.ae@gmail.com>
2020-05-18 11:59:58 +0800

    first commit

---
+++ build/CMakeFiles/message.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/message.dir/script/welcome_message.cpp.o"
+  "libmessage.pdb"
+  "libmessage.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/message.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
